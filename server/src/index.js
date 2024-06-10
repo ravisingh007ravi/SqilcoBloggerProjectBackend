b35d91084f43e0f8e716c62534182644d2ff931d
Ravi Singh <ravi6680singh@gmail.com>
2024-06-10 11:48:29 +0530

    add dependency

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,25 @@
-console.log('hbhjbjbj')
+const express = require('express');
+const mongoose = require('mongoose');
+// const route = require('./routes/route.js');
+// const dotenv = require('dotenv');
+// const cors = require('cors');
+// require('dotenv').config()
+
+const app = express();
+
+// app.use(cors());
+app.use(express.json());
+
+const DB = `mongodb+srv://DeeptirthaMukherjee:QYKI3k8QSKC4I7FZ@cluster1.khatgm1.mongodb.net/BlogsMERN=true&w=majority`;
+const port = process.env.PORT || 5000;
+
+
+//MongoDB
+mongoose.connect(DB)
+    .then(() => console.log("Mongoose is Connected😊😊"))
+    .catch((err) => console.log(err));
+
+
+// app.use('/', route);
+
+app.listen(port, () => console.log(`Server is Running Succesfully ${port}💕`));
