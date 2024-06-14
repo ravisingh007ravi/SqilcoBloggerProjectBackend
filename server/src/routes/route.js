5a001e1a4f0eff51c82fc0885dce2842d96fe61b
Ravi Singh <ravi6680singh@gmail.com>
2024-06-14 11:59:46 +0530

    createdhtml file in client folder

--- server/src/routes/route.js
+++ server/src/routes/route.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router()
 const {createUser, getAllUserData} = require('../controller/userController');
 
-//<-------------This API used for Create Author----------------------------------------->//
+//<-------------This API used for Create User-------------------------------------------->//
 router.post('/createUser',createUser)
-//<-------------This API used for Create Author----------------------------------------->//
+//<-------------This API used for Get All User Data------------------------------------->//
 router.get('/getAllData',getAllUserData)
 
 module.exports = router;
